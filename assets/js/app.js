9759c8dba4b09017ca5e28a65edd6738ac270497
Dmitry <kaprandm@gmail.com>
2021-05-01 05:40:24 +0300

    01.05.2021 finally

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
 $(function() {
 
+
     /* Nav Toggle on mobile
-    =====================================*/
+    ======================================*/
+
     let navToggle = $('#navToggle');
     let nav = $('#nav');
 
@@ -13,18 +15,19 @@ $(function() {
         nav.toggleClass('show');
     });
 
+    $(window).on("resize", function() {
+        $("body").removeClass('show-nav');
+        navToggle.removeClass('active');
+        nav.removeClass('show');
+    });
+
+
+
     let intro = $("#intro");
     let header = $("#header");
     let introH = intro.innerHeight();
     let headerH = header.innerHeight();
     let scrollTop = $(window).scrollTop();
-
-
-    $(window).on("resize", function() {
-        $("body").removeClass('show-nav');
-        $(this).toggleClass('active');
-        nav.toggleClass('show');
-    });
 
 
     /* Header class on scroll
@@ -59,8 +62,8 @@ $(function() {
 
         let scrollEl = $(this).data("scroll");
         let scrollElPos = $(scrollEl).offset().top;
-        
-        $("body").toggleClass('show-nav');
+
+        $("body").removeClass('show-nav');
         navToggle.removeClass('active');
         nav.removeClass('show');
 
@@ -117,7 +120,7 @@ $(function() {
                 transform: 'scale(1)',
                 opacity: '1'
             });
-        });
+        }, 100);
     });
 
 
@@ -168,9 +171,9 @@ $(function() {
         slidesToScroll: 1,
         arrows: false,
         fade: true,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 4000,
-        speed: 500
+        speed: 1000
     });
 
 
@@ -193,7 +196,38 @@ $(function() {
         slidesToScroll: 1,
         arrows: false,
         dots: true,
-        speed: 500
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+
+
+
+
+
+    /*  Aos.js
+        https://github.com/michalsnik/aos
+    ===============================================*/
+
+    AOS.init({
+      disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+      offset: 80, // offset (in px) from the original trigger point
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 700, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: false, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
     });
 
 
